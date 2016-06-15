cec9054f1e21ec15ce105a7b26c9979510d28ff3
supernoir <kawohl.bentodesign@gmail.com>
2016-06-16 00:23:40 +0200

    builds characterList Component, binds data to single card

--- app/app.js
+++ app/app.js
@@ -1,71 +1,57 @@
 'use strict'
-const greet = "App running with ES2015";
 
-class CharacterCard extends React.Component {
-  constructor() {
-    super();
-  }
-  
-  render() {
+let CharacterList = React.createClass({
+  getInitialState: function() {
+    return {
+      first_name: '',
+      last_name: '',
+      age: '',
+      origin: '',
+      gender: ''    
+    };
+  },
+
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.source, function (result) {
+      let characterData = result[12];
+      this.setState({
+        first_name: characterData.first_name,
+        last_name: characterData.last_name,
+        age: characterData.age,
+        origin: characterData.origin,
+        gender: characterData.gender
+      });
+    }.bind(this));
+  },
+
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
+
+  render: function() {
     return (
-<div className="ui card">
-  <div className="image">
-    <img src="http://semantic-ui.com/images/avatar2/large/kristy.png" />
-  </div>
-  <div className="content">
-    <a className="header">Kristy</a>
+      <div className="ui card">
+        <div className="content">
+          <a className="header">{this.state.first_name} {this.state.last_name} <i className={"" + this.state.gender + " icon"}></i></a>
     <div className="meta">
-      <span className="date">Joined in 2013</span>
+      <span className="date"><strong>Birthday</strong> {this.state.age}<br /><strong>Place of Origin</strong> {this.state.origin}</span>
     </div>
     <div className="description">
-      Kristy is an art director living in New York.
+      Part of the <a href="#">Nomad</a> Book
     </div>
   </div>
   <div className="extra content">
     <a>
       <i classNam="user icon"></i>
-      22 Friends
+      22 Relations
     </a>
   </div>
 </div>
     );
   }
-}
+});
 
 ReactDOM.render(
-  <CharacterCard />,
+  <CharacterList source="http://localhost:7878/characters" />,
   document.getElementById('component_characters')
 );
-
-
-fetch('http://localhost:7878/characters')  
-  .then(  
-    function(response) {  
-      if (response.status !== 200) {  
-        console.log('Looks like there was a problem. Status Code: ' +  
-          response.status);  
-        return;  
-      }
-
-      // Examine the text in the response  
-      response.json().then(function(data) {
-          for (let i = 0; i <= data.length; i++) {
-
-
-            let node = document.createElement("li");
-            let textnode = document.createTextNode(data[i].first_name + " " + data[i].last_name);
-            node.appendChild(textnode);
-            document.getElementById("character_name").appendChild(node);
-            console.log(data[i]);
-            }  
-      });  
-    }  
-  )  
-  .catch(function(err) {  
-    console.log('Fetch Error :-S', err);  
-  });
-
-
-
-
-  console.log(greet);
