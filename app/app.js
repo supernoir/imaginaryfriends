76f488b46a8c341150cd27388d3e2ce1ff6c5815
supernoir <kawohl.bentodesign@gmail.com>
2016-06-15 23:11:57 +0200

    adds react, creates first semantic ui based layout

--- app/app.js
+++ app/app.js
@@ -1,8 +1,40 @@
+'use strict'
 const greet = "App running with ES2015";
 
+class CharacterCard extends React.Component {
+  constructor() {
+    super();
+  }
+  
+  render() {
+    return (
+<div className="ui card">
+  <div className="image">
+    <img src="http://semantic-ui.com/images/avatar2/large/kristy.png" />
+  </div>
+  <div className="content">
+    <a className="header">Kristy</a>
+    <div className="meta">
+      <span className="date">Joined in 2013</span>
+    </div>
+    <div className="description">
+      Kristy is an art director living in New York.
+    </div>
+  </div>
+  <div className="extra content">
+    <a>
+      <i classNam="user icon"></i>
+      22 Friends
+    </a>
+  </div>
+</div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('characters')
+  <CharacterCard />,
+  document.getElementById('component_characters')
 );
 
 
